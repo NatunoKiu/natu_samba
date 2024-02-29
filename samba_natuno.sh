af5f28d50cb2b34985158671306e525abe1c8a08
NatunoKiu <natukiu7272@gmail.com>
2024-02-29 22:46:39 +0900

    samba_natuno.shの後ろにコンテナイメージを指定する引数を追加。

--- samba_natuno.sh
+++ samba_natuno.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo podman run -d -p 445:445 --name samba_natuno -v /mnt/share:/home/share
+sudo podman run -d -p 445:445 --name samba_natuno -v /mnt/share:/home/share localhost/sambanatuno
